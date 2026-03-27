377cb84eacee2e5e427f54c7f76dd48eb27fa1fd
Sebastian Aguilar <sebastianaguilar@Sebastians-Laptop.local>
2026-03-27 16:31:34 -0500

    create public portfolio

--- portfolio.py
+++ portfolio.py
@@ -1,7 +1,6 @@
-portfolio = {
-    "IWDA.AS": {"shares": 0.6786, "name": "All-World"},
-    "VOO": {"shares": 0.5731, "name": "S&P 500"},
-    "SCHD": {"shares": 4.9003, "name": "US Dividend Equity"},
-    "GLDM": {"shares": 0.8357, "name": "Physical Gold"},
-    "BND": {"shares": 1.0258, "name": "Bond Market"}
+public_portfolio = {
+    "VOO": {"name": "S&P 500", "shares": 10},
+    "IWDA.AS": {"name": "All World", "shares": 10},
+    "SCHD": {"name": "Dividend ETF", "shares": 10},
+    "GLD": {"name": "Gold ETF", "shares": 10},
 }
